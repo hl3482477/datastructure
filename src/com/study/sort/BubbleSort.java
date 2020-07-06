b9c1c9e49aa419f04359b6d530b5dfcf3a2135fe
hulu <1015860924@qq.com>
2020-07-06 10:27:08 +0800

    冒泡排序

---
+++ src/com/study/sort/BubbleSort.java
@@ -0,0 +1,59 @@
+package com.study.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * @author HULU
+ * @version 创建时间：2020/7/6 9:53
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+       /* int arr[] = {3, 9, -1, 10, 20};
+        System.out.println("排序前");
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+
+        System.out.println("排序后");
+        System.out.println(Arrays.toString(arr));*/
+
+        int[] arr = new int[80000];
+        for(int i =0; i < 80000;i++) {
+            arr[i] = (int)(Math.random() * 8000000); //生成一个[0, 8000000) 数
+        }
+
+        Date data1 = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date1Str = simpleDateFormat.format(data1);
+        System.out.println("排序前的时间是=" + date1Str);
+
+        //测试冒泡排序
+        bubbleSort(arr);
+
+        Date data2 = new Date();
+        String date2Str = simpleDateFormat.format(data2);
+        System.out.println("排序后的时间是=" + date2Str);
+    }
+
+    public static void bubbleSort(int[] arr) {
+
+        int temp = 0;
+        boolean flag = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - j; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            if (!flag) {//在一趟排序中，一次交换都没有发生过
+                break;
+            } else {
+                flag = false;
+            }
+        }
+    }
+}
