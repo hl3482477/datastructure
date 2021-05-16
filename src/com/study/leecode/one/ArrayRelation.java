2842126ca9298cc2e846bad0ec8228d41c84db52
hulu <hulu@codemao.cn>
2021-05-16 15:03:26 +0800

    2021-5-16

--- src/com/study/leecode/one/ArrayRelation.java
+++ src/com/study/leecode/one/ArrayRelation.java
@@ -1,6 +1,8 @@
 package com.study.leecode.one;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author HULU
@@ -9,10 +11,15 @@ import java.util.Arrays;
 public class ArrayRelation {
 
     public static void main(String[] args) {
-        /*int i = missingNumber(new int[]{0, 1, 2, 4, 5});
-        System.out.println(i);*/
-        double a = 1.0000000149011612;
-        System.out.println(a>0 && a<1);
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        addTwoNumbers(l1, l2);
+
     }
 
     /**
@@ -31,13 +38,101 @@ public class ArrayRelation {
             if (nums[mid] == mid) {
                 left = mid + 1;
             } else {
-               right =mid;
+                right = mid;
             }
         }
         return left;
     }
 
     /**
-     * 给定一个数组nums，查找一个元素，使其比排在他前面的元素都比他大
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
      */
+    public static int[] twoSum(int[] nums, int target) {
+        //方法一，时间复杂度过高
+       /* for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return  new int[]{};*/
+
+        //方法二 hashmap   空间换时间
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    /**
+     * 给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+     * <p>
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/add-two-numbers
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode returnNode = new ListNode(0);
+        ListNode listNode = returnNode;
+        //变量保存进位值
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int sum = v1 + v2 + carry;
+            listNode.next = new ListNode(sum % 10);
+            listNode=listNode.next;
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            listNode.next = new ListNode(carry);
+        }
+        return returnNode.next;
+    }
+
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
 }
